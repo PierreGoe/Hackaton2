e36c77febda851536281859f90583b6eaf0f250b
NacymeG <nacyme.gallel@gmail.com>
2022-01-20 11:55:54 +0100

    Carrelage page creation

---
+++ src/Pages/CarrelagePage.jsx
@@ -0,0 +1,13 @@
+import Carrelage from 'Componente/Carrelage';
+import MapLeaflet from 'Componente/Module/map';
+import Carrelage2 from 'Componente/Carrelage2';
+
+export default function CarrelagePage() {
+  return (
+    <>
+      <Carrelage />
+      <MapLeaflet />
+      <Carrelage2 />
+    </>
+  );
+}
