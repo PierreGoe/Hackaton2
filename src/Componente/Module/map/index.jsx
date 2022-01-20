45505490b7a160aef18a28d110ff3b9b6d999bc3
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-20 11:13:39 +0100

    Map integration

--- src/Componente/Module/map/index.jsx
+++ src/Componente/Module/map/index.jsx
@@ -1,17 +1,22 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import './style.css';
 
 export default function MapLeaflet() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <div className="container">
+        <div className="flex">
+          <h3>Besoin d&apos;un Pro ?</h3>
+          <p>Localise toi pour trouver les plus proche.</p>
+          <MapContainer center={[50.629, 2.987]} zoom={15} scrollWheelZoom>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[50.629, 2.987]} />
+          </MapContainer>
+        </div>
+      </div>
+    </>
   );
 }
