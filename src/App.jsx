157c8cc18a9119786cc61ba5f9a4063831832ab3
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-01-20 15:05:53 +0100

    Made function to get url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import CarrelagePage from 'Pages/CarrelagePage';
-import 'style.css';
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import 'style.css';
 
 function App() {
+  const [currUrl, setCurrUrl] = useState('');
+  const [urlParams, setUrlParams] = useState('');
+
+  useEffect(() => {
+    setCurrUrl(window.location.toString());
+    setUrlParams(currUrl.split('/'));
+    console.log(urlParams[3]);
+  }, [currUrl]);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<CarrelagePage />} />
+        <Route path="/carrelage" element={<CarrelagePage />} />
       </Routes>
     </>
   );
